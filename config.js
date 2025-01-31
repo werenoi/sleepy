0e3feaf3add51f269a6271d5a04357c1712c49d5
werenoi <140665557+werenoi@users.noreply.github.com>
2025-01-31 02:28:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        buyer: '1071482467938205766', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'Paranoid', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
